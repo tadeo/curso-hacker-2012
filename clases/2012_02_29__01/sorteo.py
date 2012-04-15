a844a28d0b325ef26a522e7fb6fb49b8c5e331a3
tadeo <tadeo@tadeocarrier.com>
2012-04-15 20:18:46 -0300

    Script del sorteo inicial en python

---
+++ clases/2012_02_29__01/sorteo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import random
+list = [u'Rafael Gallareto', u'Marcos Irureta Goyena', u'Santiago Fagnoni', u'maui (Nicolás Rodríguez)', u'Santiago Curbelo', u'Fernando Pose', u'Coli Quijano', u'kbz (Pablo Montedónico)', u'Fabian Molina']
+random.shuffle(list)
+print list
+#['Coli Quijano', 'Santiago Curbelo', 'Fernando Pose', 'maui (Nicols Rodrguez)', 'Rafael Gallareto', 'Marcos Irureta Goyena', 'kbz (Pablo Montednico)', 'Fabian Molina', 'Santiago Fagnoni']
