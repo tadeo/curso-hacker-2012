9197d4453f0dbd278c5e17cca7daa009e7032b68
kbz <kbz@kbz.com.uy>
2012-04-23 22:33:48 -0300

    kbz adjust name vars

--- clases/2012_04_23__08/loremipsum/kbz.py
+++ clases/2012_04_23__08/loremipsum/kbz.py
@@ -1,12 +1,12 @@
 #	2012 04 23
 #	author: [kbz]: www.kbz.com.uy
 
-myLorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+my_lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
 
-def loremWords (howWords):	
-	words = myLorem.split(' ')
+def lorem_words (how_words):	
+	words = my_lorem.split(' ')
 	
-	result = words[:howWords]
+	result = words[:how_words]
 	return ' '.join(result)
 
-loremWords(5)
+lorem_words(5)
