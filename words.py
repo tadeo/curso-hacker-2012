094e513fa53a89366bafdee9ddb9e281992aa084
Gerardo <pituuy@yahoo.com.ar>
2012-05-07 19:14:09 -0300

    ejercicio gerardo

---
+++ words.py
@@ -0,0 +1,8 @@
+x='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit erat non metus consectetur sit amet feugiat massa ultrices. Nulla in eros id purus condimentum posuere. Donec hendrerit urna id nibh ornare laoreet volutpat enim laoreet. Curabitur venenatis eleifend ipsum non molestie. Donec metus enim, sollicitudin non cursus ac, dignissim sit amet ipsum. Cras accumsan erat sit amet eros aliquam. '
+
+lor=x.split(' ')
+def lorem(y):
+	s=lor[:y]
+	r=' '.join(s)
+	return r
+		
