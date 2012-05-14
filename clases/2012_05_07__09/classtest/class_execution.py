c352cc1bfad11960fbc9e0f550f9bbcd192e143b
tadeo <tadeo@tadeocarrier.com>
2012-05-14 19:11:39 -0300

    classtest package created

---
+++ clases/2012_05_07__09/classtest/class_execution.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from class_definition import Basket as Canasta
+from hash_password import hash_password
+#from class_definition import *
+#import class_definition
+
+
+basket4 = Canasta([1,2,3,4])
+print '>>>>>>>>>>>: %s' % basket4
+
+
+#hash_password('test')
